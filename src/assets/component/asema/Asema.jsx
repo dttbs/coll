53ef5875c7753b51240f1fa74f139a2c4e4eaf09
ae0108 <asema.eshpaeva@gmail.com>
2025-04-26 09:15:04 +0600

    commit

--- src/assets/component/asema/Asema.jsx
+++ src/assets/component/asema/Asema.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import myImage1 from "../../img/asema.png"
 
 export const Asema = () => {
-  return <div>Asema</div>;
+  return <div  >
+    <h1 className="text-red-500">Мы любим поиграть</h1>
+    <img src={myImage1} alt="" classNameAsema=""  />
+  </div>;
 };
