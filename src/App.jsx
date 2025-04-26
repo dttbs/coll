beb71e64a79cc7c9bba7e8df06a33910c8a566bf
ssanjar07 <fortyn77701@gmail.com>
2025-04-26 11:14:53 +0600

    Закоммитил изменения перед pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-
 import { Hanif } from "./assets/component/hanif/Hanif";
-import { Section } from "./assets/component/asemaK/Section"
-import { Asema } from "./assets/component/asema/Asema"
-
+import { Section } from "./assets/component/asemaK/Section";
+import { Asema } from "./assets/component/asema/Asema";
+import { Sanjar } from "./assets/component/sanjar/Sanjar";
 
 function App() {
-
   return (
     <>
       <Hanif />
-    <Section />
+      <Section />
       <Asema />
+      <Sanjar />
     </>
   );
 }
 
 export default App;
-      
-
